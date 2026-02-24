6c09b6dea9e7bb7c06cb08d2d15ae51b018556e6
Vito <vito.licari78@gmail.com>
2026-02-24 05:57:20 +0100

    Conversione in webp

--- components/progetti/Carousel.jsx
+++ components/progetti/Carousel.jsx
@@ -15,7 +15,7 @@ const BG_BY_ID = {
   atletica: "/backgrounds/bgCardAtletica.webp",
   piscine: "/backgrounds/bgCardPiscine.webp",
   campi: "/backgrounds/bgCardCampi.webp",
-  africa: "/projects/africa/bgCardVillaggio.png",
+  africa: "/backgrounds/bgCardVillaggio.webp",
 };
 
 function cx(...classes) {
